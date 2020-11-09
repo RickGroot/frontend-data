12c55a88ae52a1624dbd5435196b9e76c9737604
Rick Groot <RickGroot@gmail.com>
2020-11-09 10:10:37 +0100

    modules session

--- d3test/script.js
+++ d3test/script.js
@@ -1,6 +1,6 @@
 //local server command: python -m http.server
-
-import { endpoint, endpoint2, selectedColumn, selectedColumn2 } from './modules/data.js';
+import { endpoint, endpoint2, selectedColumn, selectedColumn2 } from './modules/endpoint.js';
+import { compare } from './modules/array.js';
 
 let data1 = getData(endpoint) //calls function getData with API link
     .then(result => { //only continues when data is fetched
@@ -58,38 +58,6 @@ function filterObjectValue(dataArray, key) {
 function filterObjectName(dataArray, key) {
     return dataArray.filter(item => item[key] === 'GARAGEP'); //returns only objects with a key-value higher than 0
 }
-
-async function compare(array1, array2) { //async function that awaits the promised arrays
-    const result1 = await array1; //waits for incoming data
-    const result2 = await array2;
-    let compiled = [];
-
-    result1.forEach(itemArr1 => { //loops over each item in result1
-
-        result2.forEach(itemArr2 => { //loops over each item in result2 to check same area id's
-
-            if (itemArr1.areaid === itemArr2.areaid) {
-
-                let location = [parseFloat(itemArr2.location.longitude), parseFloat(itemArr2.location.latitude)]; //saves location to an array
-
-                compiled.push({ //pushes an object into array 'compiled'
-                    areamanagerId: itemArr1.areamanagerid,
-                    areaId: itemArr1.areaid,
-                    capacity: parseFloat(itemArr1.capacity), //parseFloat() turns a string containing a number to a number type
-                    chargingpointCapacity: itemArr1.chargingpointcapacity,
-                    disabledAccess: itemArr1.disabledaccess,
-                    maximumVehicleHeight: itemArr1.maximumvehicleheight,
-                    limitedAccess: itemArr1.limitedaccess,
-                    location: location,
-                    areaDesc: itemArr2.areadesc
-                });
-            }
-        })
-    })
-
-    return compiled;
-}
-
 
 //D3 code
 let mapSVG = d3.select("#map");
